57658cc4230892d947e0dbaeb0248abf80d2d529
Jason <18610599620@163.com>
2024-03-01 22:40:21 +0800

    Add:添加sh脚本 * 修改gitignore * 添加sh脚本

---
+++ deepin_wechat_shadow_killer.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+/usr/bin/nohup /home/Jason/.pyenv/shims/python3 /home/Jason/.shell/deepin_wechat_shadow_killer/deepin_wechat_shadow_killer.py > /home/Jason/.shell/deepin_wechat_shadow_killer/log.txt &
